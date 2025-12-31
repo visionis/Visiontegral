b6f335ad0f6e96211a5f60130320e46bb516303b
efe <168096049+visionis@users.noreply.github.com>
2025-12-31 17:29:05 +0300

    Update __init__.py

--- visualization/__init__.py
+++ visualization/__init__.py
@@ -1,1 +1,17 @@
+"""
+Visiontegral Visualization Suite
+Author: Visionis
+Version: 1.0.0
+"""
+
+from .animator import VisiontegralScene, ThemeConfig, RenderSettings
+from .renderer_3d import ManifoldExplorer
+from .color_maps import VisionColorMapper, PaletteVault
+
+__all__ = [
+    "VisiontegralScene",
+    "ManifoldExplorer",
+    "VisionColorMapper",
+    "PaletteVault"
+]
 
